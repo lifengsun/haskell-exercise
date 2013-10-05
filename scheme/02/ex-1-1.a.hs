777dad0a10c54793fbc812c5ce0063629270b4dc
Lifeng Sun <lifongsun@gmail.com>
2013-10-05 16:30:58 +0800

    Write Yourself a Scheme in 48 Hours: chapter 2.

---
+++ scheme/02/ex-1-1.a.hs
@@ -0,0 +1,14 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseNumber :: Parser LispVal
+parseNumber = do
+  s <- many1 digit
+  return . Number . read $ s
---
+++ scheme/02/ex-1-1.b.hs
@@ -0,0 +1,12 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseNumber :: Parser LispVal
+parseNumber = many1 digit >>= return . Number . read
