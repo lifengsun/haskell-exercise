5e1af0cebb287488d87df408a0d2ea9b8445ba00
Lifeng Sun <lifongsun@gmail.com>
2013-10-06 12:57:56 +0800

    upenn CIS 194 - Week 11: finished.

--- upenn-cis-194/11/SExpr.hs
+++ upenn-cis-194/11/SExpr.hs
@@ -6,26 +6,27 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> (pure [])
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = (:) <$> p <*> zeroOrMore p
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore (char ' ')
 
 ident :: Parser String
-ident = undefined
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -44,3 +45,9 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+atom :: Parser SExpr
+atom = A <$> (spaces *> ((N <$> posInt) <|> (I <$> ident)) <* spaces)
+
+parseSExpr :: Parser SExpr
+parseSExpr = atom <|> char '(' *> (Comb <$> (zeroOrMore parseSExpr)) <* char ')'
